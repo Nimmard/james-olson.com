95afc8d8fb6f502c525501c617e563726486295f
James Olson <nimmard@gmail.com>
2013-10-22 08:50:25 -0700

    Added post archives, rearranged some code to prevent repetition.

--- blog/templatetags/entryfilters.py
+++ blog/templatetags/entryfilters.py
@@ -3,11 +3,11 @@ from django import template
 register = template.Library()
 
 @register.filter
-def shortenEntry(value):
+def shortenEntry(value, post):
     value_list = [str(x) for x in value.split(" ")]
     if len(value) >= 45:
         shortened = ' '.join(value_list[:45]) 
-        return shortened + "..."
+        return shortened + "<a href='%s'>...</a>" % post
     else:
         return value
 
--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,44 @@
 from blog.models import Entries, Category
 from django.views.generic import ListView, DetailView
 from django.views.generic.dates import MonthArchiveView,YearArchiveView
-
-
-class BlogDetailView(DetailView):
-    model = Entries
-    context_object_name = "entry"
+from calendar import month_name
 
 class BlogMenuMixin(object):
     
+    def get_blog_archives(self):
+        edates = Entries.objects.dates('created', 'month')
+        archivedict = dict()
+        entries_list = Entries.objects.all()
+        for entrydate in edates:
+            monthname = month_name[entrydate.month]
+            if entrydate.year in archivedict:
+                archivedict[entrydate.year].update({ monthname : entries_list.filter(
+                    created__year=entrydate.year,
+                    created__month=entrydate.month).count()})
+            else:
+                archivedict[entrydate.year] = {
+                        monthname : entries_list.filter(
+                            created__year=entrydate.year,
+                            created__month=entrydate.month).count()
+                            }
+        return archivedict
     def get_context_data(self, **kwargs):
         context = super(BlogMenuMixin, self).get_context_data(**kwargs)
         context['categories'] = set(Category.objects.filter(entries__isnull=False))
+        context['archives'] = self.get_blog_archives()
 
         return context
 
+class BlogDetailView(BlogMenuMixin, DetailView):
+    model = Entries
+    context_object_name = "entry"
+
+
 class BlogListView(BlogMenuMixin, ListView):
     template_name="blog/blog_list.html"
-    model = Entries
+    queryset = Entries.objects.all().select_related()
     context_object_name = "entries"
+
 
 class CategoryListView(BlogMenuMixin, ListView):
     template_name="blog/blog_list.html"
