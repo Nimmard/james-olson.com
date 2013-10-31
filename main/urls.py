f0cc6cff9bda0282079ab56b8e9c7c3e985e6b47
James Olson <nimmard@gmail.com>
2013-10-31 05:34:44 -0700

    Started custom admin app, fixed URL (again)

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,5 @@ from main.views import SiteIndexView
 from main.views import contact
 urlpatterns = patterns('',
         url(r'^contact/$', contact, name="contact"),
-        url(r'/$', SiteIndexView.as_view(), name="siteindex"),
+        url(r'^$', SiteIndexView.as_view(), name="siteindex"),
         )
