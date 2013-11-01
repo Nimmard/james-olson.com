2406dedfe356eca34635eaee946047e3aa213953
James Olson <nimmard@gmail.com>
2013-10-31 23:29:48 -0700

    Removed API call to github, hardcoded repos. Was causing too much page loading time.

--- main/views.py
+++ main/views.py
@@ -3,8 +3,6 @@ from django.views.generic import TemplateView
 import feedparser
 from blog.models import Entries
 from bs4 import BeautifulSoup
-import requests
-from settings import settings
 from main.forms import ContactForm
 from django.http import HttpResponse
 from django.http import Http404
@@ -12,8 +10,6 @@ class SiteIndexView(TemplateView):
     template_name = 'main/index.html'
 
     def get_context_data(self, **kwargs):
-        username = 'Nimmard'
-        personal_token = settings.GITHUB_PERSONAL_TOKEN
         feed = feedparser.parse('https://github.com/Nimmard.atom')
 
         entries = feed.entries
@@ -31,8 +27,6 @@ class SiteIndexView(TemplateView):
         context = super(SiteIndexView, self).get_context_data(**kwargs)
         context['entries'] = Entries.objects.order_by('-created')[:4]
         context['commits'] = git_commits
-        context['github_user'] = requests.get('https://api.github.com/user', auth=(username, personal_token)).json()
-        context['github_repos'] = requests.get('https://api.github.com/users/Nimmard/repos', auth=(username, personal_token)).json()
         return context
 
 def contact(request):
