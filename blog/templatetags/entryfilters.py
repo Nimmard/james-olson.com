7c1e0b8886f8ad1427fe3b809c304cb652ca6b2f
James Olson <nimmard@gmail.com>
2014-02-18 16:06:51 -0800

    Few small changes to the rendering of blog excerpts.

--- blog/templatetags/entryfilters.py
+++ blog/templatetags/entryfilters.py
@@ -12,8 +12,8 @@ def markdowner(value):
 @register.filter
 def shortenEntry(value, post):
     value_list = [str(x) for x in value.split(" ")]
-    if len(value) >= 45:
-        shortened = ' '.join(value_list[:45]) 
+    if len(value) >= 600:
+        shortened = ' '.join(value_list[:600]) 
         return shortened + "<a href='%s'>...</a>" % post
     else:
         return value
